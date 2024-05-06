30201beb0facbf92aee09449e41bad471e7ff903
Hoc vien <nhnhan2102@gmail.com>
2024-05-06 15:51:07 +0700

    Test Code for Jenskins

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GradeConverterTest {
+
+  private GradeConverter converter;
+
+  @Before
+  public void setUp() throws Exception {
+    converter = new GradeConverter();
+  }
+
+  @Test
+  public void testConverter1() {
+    String expected = "A";
+    String actual = converter.convert(100);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testConverter2() {
+    String expected = "A";
+    String actual = converter.convert(90);
+    assertEquals(expected, actual);
+  }
+}
